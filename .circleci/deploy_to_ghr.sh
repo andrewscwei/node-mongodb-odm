46464c5f9a869549a7c1165a2e2b64b473bf43b3
Andrew Wei <andrewscwei@gmail.com>
2018-10-24 12:10:31 -0700

    Update CI script

---
+++ .circleci/deploy_to_ghr.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "Successfully published to GitHub releases"
--- .circleci/deploy_to_npm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "//registry.npmjs.org/:_authToken=$NPM_AUTH" >> ~/.npmrc
-npm publish
-echo "Successfully published to NPM"
