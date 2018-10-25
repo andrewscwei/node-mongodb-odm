b36a5ac1e1cdd2c726e0cefadbc31e417d8fb846
Andrew Wei <andrewscwei@gmail.com>
2018-10-24 18:55:47 -0700

    Test deploy

--- .circleci/deploy_to_ghr.sh
+++ .circleci/deploy_to_ghr.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -delete ${CIRCLE_TAG} ./package/
+
 echo "Successfully published to GitHub releases"
