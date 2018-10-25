c9612e02952a61c8f19ae41234d4aad1f92c6ca5
Andrew Wei <andrewscwei@gmail.com>
2018-10-25 00:04:25 -0700

    Update CI script

--- .circleci/deploy_to_ghr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-go get github.com/tcnksm/ghr
-ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -delete ${CIRCLE_TAG} ./package/
-
-echo "Successfully published to GitHub releases"
